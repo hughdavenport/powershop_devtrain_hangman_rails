f7ae61c93c9695a8e59a2474ffdafa88a0231755
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-03-09 16:40:18 +1300

    Actually show validation errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    article = Article.new(article_params)
-    if article.save
-      redirect_to article
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
     else
       render 'new'
     end
