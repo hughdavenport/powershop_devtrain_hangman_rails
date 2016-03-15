e9580be39adbc6e690d32331fe113b818e973523
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-03-15 13:39:52 +1300

    Add score to game model
    Tests passing

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,9 @@
 class Game < ActiveRecord::Base
+  attr_reader :score
+
+  DEFAULT_STARTING_SCORE = 10
+
+  def initialize(starting_score: DEFAULT_STARTING_SCORE)
+    @score = starting_score
+  end
 end
