da4f4859f4502cf08f9f87e79d741a8b5886a42f
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-03-23 15:07:03 +1300

    Add spec for winning game

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -66,10 +66,11 @@ RSpec.describe Game, type: :model do
   end
 
   context "when I have a new game with 1 life left" do
-    subject(:game) { Game.new(starting_score: 1) }
+    subject(:game) { Game.new(word: word, starting_score: 1) }
+    let(:word) { "hangman" }
 
     context "and I make an incorrect guess" do
-      let(:guess) { 'z' } # default word of hangman
+      let(:guess) { 'z' }
       before { game.submit_guess(guess) }
 
       describe "the game" do
@@ -86,7 +87,7 @@ RSpec.describe Game, type: :model do
     end
 
     context "and I made a correct guess" do
-      let(:guess) { 'a' } # default word of hangman
+      let(:guess) { 'a' }
       before { game.submit_guess(guess) }
 
       describe "the game" do
@@ -99,6 +100,47 @@ RSpec.describe Game, type: :model do
         subject { game.lives }
 
         it { is_expected.to be 1 }
+      end
+    end
+  end
+
+  context "when I have a game with the word almost guessed and three incorrect guesses" do
+    subject(:game) { Game.new(word: word) }
+    let(:word) { "hangman" }
+    let(:guesses) { ["e", "g", "m", "s", "n", "h", "r"] }
+    before { guesses.each { |guess| game.submit_guess(guess) } }
+
+    context "and I make an incorrect guess" do
+      let(:guess) { 'z' }
+      before { game.submit_guess(guess) }
+
+      describe "the game" do
+        it { is_expected.not_to be_won }
+        it { is_expected.not_to be_lost }
+        it { is_expected.not_to be_finished }
+      end
+
+      describe "#lives" do
+        subject { game.lives }
+
+        it { is_expected.to be 6 }
+      end
+    end
+
+    context "and I made a correct guess" do
+      let(:guess) { 'a' }
+      before { game.submit_guess(guess) }
+
+      describe "the game" do
+        it { is_expected.to be_won }
+        it { is_expected.not_to be_lost }
+        it { is_expected.to be_finished }
+      end
+
+      describe "#lives" do
+        subject { game.lives }
+
+        it { is_expected.to be 7 }
       end
     end
   end
