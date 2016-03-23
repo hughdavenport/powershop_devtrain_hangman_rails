3ad7f99f6e5a8aefab23bd71a3449ff7477ad921
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-03-23 15:48:06 +1300

    Add validation of lower case letter

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,4 +2,5 @@ class Guess < ActiveRecord::Base
   belongs_to :game
   validates :guess, uniqueness: { scope: :game }
   validates :guess, length: { is: 1 }
+  validates :guess, format: { with: /\A[a-z]\z/ }
 end
