a1e397a3022455aa158fc5a03a9e014870aa9680
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-03-31 14:25:26 +1300

    Remove spec for game create method

--- spec/routing/games_routing_spec.rb
+++ spec/routing/games_routing_spec.rb
@@ -19,10 +19,6 @@ RSpec.describe GamesController, type: :routing do
       expect(:get => "/games/1/edit").to route_to("games#edit", :id => "1")
     end
 
-    it "routes to #create" do
-      expect(:post => "/games").to route_to("games#create")
-    end
-
     it "routes to #update via PUT" do
       expect(:put => "/games/1").to route_to("games#update", :id => "1")
     end
