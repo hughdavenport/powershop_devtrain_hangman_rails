053695ae177e27af794110437483d3760c2d3430
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-04-08 11:03:49 +1200

    Add valid and invalid tests for word list specs

--- spec/controllers/word_lists_controller_spec.rb
+++ spec/controllers/word_lists_controller_spec.rb
@@ -19,17 +19,14 @@ require 'rails_helper'
 # that an instance is receiving a specific message.
 
 RSpec.describe WordListsController, type: :controller do
+  let(:name) { "testing" }
 
   # This should return the minimal set of attributes required to create a valid
   # WordList. As you add validations to WordList, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
+  let(:valid_attributes) { { name: name } }
 
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
+  let(:invalid_attributes) { { foo: :bar } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
