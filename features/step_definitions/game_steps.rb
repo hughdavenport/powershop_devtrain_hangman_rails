9fca826c632a7252f9be7874bb24cfea58c41f05
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-04-11 10:06:46 +1200

    Make step for making guesses use generic steps

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -39,8 +39,8 @@ end
 
 When(/^I make a(n)? (in)?valid guess$/) do |plural_to_ignore, invalid|
   @guess = ((invalid ? INVALID_GUESSES : VALID_GUESSES) - @game.guessed_letters).sample
-  fill_in "guess[guess]", :with => @guess
-  click_on "Submit guess"
+  step "I enter #{@guess} as Guess"
+  step 'I click "Submit guess"'
 end
 
 When(/^I make (\d+) (in)?valid guesses$/) do |number, invalid|
