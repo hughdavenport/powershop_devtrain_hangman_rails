064558a463f08e8510d68d4f6626c57706826bd7
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-04-12 10:22:44 +1200

    Persist locale params, unless default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,12 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-      I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    options.tap do |options|
+      options[:locale] = I18n.locale unless I18n.locale == I18n.default_locale
+    end
   end
 end
